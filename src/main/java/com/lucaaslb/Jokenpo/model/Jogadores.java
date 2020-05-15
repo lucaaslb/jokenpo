7cff86043554f9acf3062cea311e58122831fc1f
Lucas Lacerda <lucaaslb@hotmail.com>
2020-05-15 13:02:51 -0300

    configurando gradle, test OK

---
+++ src/main/java/com/lucaaslb/Jokenpo/model/Jogadores.java
@@ -0,0 +1,19 @@
+package com.lucaaslb.Jokenpo.model;
+
+public class Jogadores {
+
+	private int numero;
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Jogadores(int numero) {
+		this.numero = numero;
+	}
+
+}
